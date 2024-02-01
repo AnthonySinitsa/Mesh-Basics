06f7f4debfc70bc138ed11bf8e99c97446a7a20c
Anton <itonyemail@gmail.com>
2024-02-01 14:11:48 -0800

    triangle created using points

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -7,13 +7,19 @@ public class Grid : MonoBehaviour{
 
     private Vector3[] vertices;
 
+    private Mesh mesh;
+
     private void Awake(){
         StartCoroutine(Generate());
     }
 
-
     private IEnumerator Generate(){
         WaitForSeconds wait = new WaitForSeconds(0.05f);
+
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        mesh.name = "Procedural Grid";
+
         vertices = new Vector3[(xSize + 1) * (ySize + 1)];
         for(int i = 0, y = 0; y <= ySize; y++){
             for(int x = 0; x <= xSize; x++, i++){
@@ -21,6 +27,14 @@ public class Grid : MonoBehaviour{
                 yield return wait;
             }
         }
+
+        mesh.vertices = vertices;
+
+        int[] triangles = new int[3];
+        triangles[0] = 0;
+        triangles[1] = xSize + 1;
+        triangles[2] = 1;
+        mesh.triangles = triangles;
     }
 
     // visualize vertices
