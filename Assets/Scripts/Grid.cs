5ab13c1cd7dcbd4b1df8cc71cf9dc945f7740857
Anton <itonyemail@gmail.com>
2024-02-01 15:54:13 -0800

    filing entire grid with traingles

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -30,12 +30,17 @@ public class Grid : MonoBehaviour{
 
         mesh.vertices = vertices;
 
-        int[] triangles = new int[6];
-        triangles[0] = 0;
-		triangles[3] = triangles[2] = 1;
-		triangles[4] = triangles[1] = xSize + 1;
-		triangles[5] = xSize + 2;
-        mesh.triangles = triangles;
+        int[] triangles = new int[xSize * ySize * 6];
+        for(int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++){
+            for(int x = 0; x < xSize; x++, ti += 6, vi++){
+                triangles[ti] = vi;
+		        triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+		        triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+		        triangles[ti + 5] = vi + xSize + 2;
+                mesh.triangles = triangles;
+                yield return wait;
+            }
+        }
     }
 
     // visualize vertices
