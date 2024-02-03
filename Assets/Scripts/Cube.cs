bf2f383970522bcc57af7ef2c767740e8d9b5983
Anton <itonyemail@gmail.com>
2024-02-02 16:39:53 -0800

    all veritces for cube rendered with no duplicates

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -3,11 +3,11 @@ using System.Collections;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class Cube : MonoBehaviour{
+    
     public int xSize, ySize, zSize;
 
-    private Vector3[] vertices;
-
     private Mesh mesh;
+    private Vector3[] vertices;
 
     private void Awake(){
         StartCoroutine(Generate());
@@ -28,21 +28,35 @@ public class Cube : MonoBehaviour{
         vertices = new Vector3[cornerVertices + edgeVertices + faceVertices];
 
         int v = 0;
-        for(int x = 0; x <= xSize; x++){
-            vertices[v++] = new Vector3(x, 0, 0);
-            yield return wait;
+        for(int y = 0; y <= ySize; y++){
+            for(int x = 0; x <= xSize; x++){
+                vertices[v++] = new Vector3(x, y, 0);
+                yield return wait;
+            }
+            for (int z = 1; z <= zSize; z++) {
+                vertices[v++] = new Vector3(xSize, y, z);
+                yield return wait;
+            }
+            for (int x = xSize - 1; x >= 0; x--) {
+                vertices[v++] = new Vector3(x, y, zSize);
+                yield return wait;
+            }
+            for (int z = zSize - 1; z > 0; z--) {
+                vertices[v++] = new Vector3(0, y, z);
+                yield return wait;
+            }
         }
-        for (int z = 1; z <= zSize; z++) {
-			vertices[v++] = new Vector3(xSize, 0, z);
-			yield return wait;
+        for (int z = 1; z < zSize; z++) {
+			for (int x = 1; x < xSize; x++) {
+				vertices[v++] = new Vector3(x, ySize, z);
+				yield return wait;
+			}
 		}
-		for (int x = xSize - 1; x >= 0; x--) {
-			vertices[v++] = new Vector3(x, 0, zSize);
-			yield return wait;
-		}
-		for (int z = zSize - 1; z > 0; z--) {
-			vertices[v++] = new Vector3(0, 0, z);
-			yield return wait;
+		for (int z = 1; z < zSize; z++) {
+			for (int x = 1; x < xSize; x++) {
+				vertices[v++] = new Vector3(x, 0, z);
+				yield return wait;
+			}
 		}
 
     }
