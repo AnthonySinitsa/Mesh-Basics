cffd6f9a74d2345b091a352d4221d76d6772e97b
Anton <itonyemail@gmail.com>
2024-02-02 16:29:16 -0800

    a row of vertices for cube generates

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -10,40 +10,41 @@ public class Cube : MonoBehaviour{
     private Mesh mesh;
 
     private void Awake(){
-        Generate();
+        StartCoroutine(Generate());
     }
 
-    private void Generate(){
+    private IEnumerator Generate(){
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
         mesh.name = "Procedural Cube";
+        WaitForSeconds wait = new WaitForSeconds(0.05f);
 
-        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
-        Vector2[] uv = new Vector2[vertices.Length];
-        Vector4[] tangents = new Vector4[vertices.Length];
-        Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
-        for(int i = 0, y = 0; y <= ySize; y++){
-            for(int x = 0; x <= xSize; x++, i++){
-                vertices[i] = new Vector3(x, y);
-                uv[i] = new Vector2((float)x / xSize, (float)y / ySize);
-                tangents[i] = tangent;
-            }
-        }
-        mesh.vertices = vertices;
-        mesh.uv = uv;
-        mesh.tangents = tangents;
+        int cornerVertices = 8;
+        int edgeVertices = (xSize + ySize + zSize - 3) * 4;
+        int faceVertices = (
+            (xSize - 1) * (ySize - 1) +
+            (xSize - 1) * (zSize - 1) +
+            (ySize - 1) * (zSize)) * 2;
+        vertices = new Vector3[cornerVertices + edgeVertices + faceVertices];
 
-        int[] triangles = new int[xSize * ySize * 6];
-        for(int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++){
-            for(int x = 0; x < xSize; x++, ti += 6, vi++){
-                triangles[ti] = vi;
-		        triangles[ti + 3] = triangles[ti + 2] = vi + 1;
-		        triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
-		        triangles[ti + 5] = vi + xSize + 2;
-            }
+        int v = 0;
+        for(int x = 0; x <= xSize; x++){
+            vertices[v++] = new Vector3(x, 0, 0);
+            yield return wait;
         }
-        mesh.triangles = triangles;
-        mesh.RecalculateNormals();
+        for (int z = 1; z <= zSize; z++) {
+			vertices[v++] = new Vector3(xSize, 0, z);
+			yield return wait;
+		}
+		for (int x = xSize - 1; x >= 0; x--) {
+			vertices[v++] = new Vector3(x, 0, zSize);
+			yield return wait;
+		}
+		for (int z = zSize - 1; z > 0; z--) {
+			vertices[v++] = new Vector3(0, 0, z);
+			yield return wait;
+		}
+
     }
 
     // visualize vertices
@@ -52,7 +53,6 @@ public class Cube : MonoBehaviour{
         if (vertices == null) {
 			return;
 		}
-
         Gizmos.color = Color.black;
         for(int i = 0; i < vertices.Length; i++){
             Gizmos.DrawSphere(vertices[i], 0.1f);
