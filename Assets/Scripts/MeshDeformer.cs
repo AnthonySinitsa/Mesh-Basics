fb8160a66e7f177818dada245a97058d45d2ce5a
Anton <itonyemail@gmail.com>
2024-02-07 14:46:13 -0800

    vertex velocities

--- Assets/Scripts/MeshDeformer.cs
+++ Assets/Scripts/MeshDeformer.cs
@@ -6,6 +6,8 @@ public class MeshDeformer : MonoBehaviour {
     Mesh deformingMesh;
     Vector3[] originalVertices, displacedVertices;
 
+    Vector3[] vertexVelocities;
+
     void Start () {
 		deformingMesh = GetComponent<MeshFilter>().mesh;
 		originalVertices = deformingMesh.vertices;
@@ -13,5 +15,6 @@ public class MeshDeformer : MonoBehaviour {
 		for (int i = 0; i < originalVertices.Length; i++) {
 			displacedVertices[i] = originalVertices[i];
 		}
+        vertexVelocities = new Vector3[originalVertices.Length];
 	}
 }
