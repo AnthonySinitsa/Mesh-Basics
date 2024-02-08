ec916f151ce8bfbaa296ca2867220efece3f01a9
Anton <itonyemail@gmail.com>
2024-02-07 17:20:30 -0800

    adjusting scale

--- Assets/Scripts/MeshDeformer.cs
+++ Assets/Scripts/MeshDeformer.cs
@@ -9,6 +9,7 @@ public class MeshDeformer : MonoBehaviour {
     Vector3[] vertexVelocities;
     public float springForce = 20f;
     public float damping = 5f;
+    float uniformScale = 1f;
 
     void Start () {
 		deformingMesh = GetComponent<MeshFilter>().mesh;
@@ -31,10 +32,11 @@ public class MeshDeformer : MonoBehaviour {
     void UpdateVertex(int i){
         Vector3 velocity = vertexVelocities[i];
         Vector3 displacement = displacedVertices[i] - originalVertices[i];
+        displacement *= uniformScale;
         velocity -= displacement * springForce * Time.deltaTime;
         velocity *= 1f - damping * Time.deltaTime;
         vertexVelocities[i] = velocity;
-        displacedVertices[i] += velocity * Time.deltaTime;
+        displacedVertices[i] += velocity * (Time.deltaTime / uniformScale);
     }
 
     public void AddDeformingForce(Vector3 point, float force){
@@ -46,6 +48,7 @@ public class MeshDeformer : MonoBehaviour {
 
     void AddForceToVertex(int i, Vector3 point, float force){
         Vector3 pointToVertex = displacedVertices[i] - point;
+        pointToVertex *= uniformScale;
         float attenuateForce = force / (1f + pointToVertex.sqrMagnitude);
         float velocity = attenuateForce * Time.deltaTime;
         vertexVelocities[i] += pointToVertex.normalized * velocity;
