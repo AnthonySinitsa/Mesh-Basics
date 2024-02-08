c7bf8d59da00619b6c496935dce8a8340c64ab90
Anton <itonyemail@gmail.com>
2024-02-07 16:07:32 -0800

    detecting input

--- Assets/Scripts/MeshDeformerInput.cs
+++ Assets/Scripts/MeshDeformerInput.cs
@@ -3,4 +3,19 @@ using UnityEngine;
 public class MeshDeformerInput : MonoBehaviour{
 
     public float force = 10f;
+
+    void Update(){
+        if(Input.GetMouseButton(0)){
+            HandleInput();
+        }
+    }
+
+    void HandleInput(){
+        Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(physics.Raycast(inputRay, out hit)){
+            MeshDeformer deformer = hit.collider.GetComponent<MeshDeformer>();
+        }
+    }
 }
