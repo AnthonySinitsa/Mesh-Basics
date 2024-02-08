0208a2fac9dae34beb77099d8f647a587b783f30
Anton <itonyemail@gmail.com>
2024-02-07 16:13:09 -0800

    applying force

--- Assets/Scripts/MeshDeformerInput.cs
+++ Assets/Scripts/MeshDeformerInput.cs
@@ -14,8 +14,12 @@ public class MeshDeformerInput : MonoBehaviour{
         Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
-        if(physics.Raycast(inputRay, out hit)){
+        if(Physics.Raycast(inputRay, out hit)){
             MeshDeformer deformer = hit.collider.GetComponent<MeshDeformer>();
+            if(deformer){
+                Vector3 point = hit.point;
+                deformer.AddDeformingForce(point, force);
+            }
         }
     }
 }
